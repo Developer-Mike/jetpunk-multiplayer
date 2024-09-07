f4626bebf9dcfca85b3c2f528d49644e2388f266
Developer-Mike <delta.cephei@outlook.com>
2024-09-07 18:36:01 +0200

    Ready for deployment

--- post-build.bat
+++ post-build.bat
@@ -3,13 +3,6 @@
 REM Copy the styles.css file to the dist folder
 copy .\src\client\styles.css .\dist\client\styles.css
 
-REM Copy the .env file to the dist folder
-copy .\.env .\dist\.env
-
 REM Make tampermonkey script variant with filled placeholders
 REM Read .\dist\client\index.user.js, Replace SERVER_URL with http://localhost:3000", Save to .\dist\client\index.hr.user.js
 powershell -Command "(gc .\dist\client\index.user.js) -replace 'SERVER_URL', 'http://localhost:3000' | Out-File -encoding ASCII .\dist\client\index.hr.user.js"
-
-REM Start the server
-cd .\dist
-node .\index.js
